52cb73ac307fe7c376238a60afed6dce89d4e889
gustim <augustobormoreira@gmail.com>
2025-07-11 16:05:25 -0300

    Project Started, Initial Interface with Java Swing implementation

---
+++ blackjack/src/main/java/blackjackpoo/blackjack/Blackjack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package blackjackpoo.blackjack;
+
+/**
+ *
+ * @author Gustim
+ */
+public class Blackjack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
